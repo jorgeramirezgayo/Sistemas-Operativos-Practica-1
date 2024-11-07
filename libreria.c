47ae4bc0f0de26c0a55ae0c676f0a54bee211c18
jorgeramirezgayo <jorgeramirezgayo@outlook.es>
2024-11-07 02:00:18 +0100

    Compile test

--- libreria.c
+++ libreria.c
@@ -1,11 +1,19 @@
-int head (int N) {
+#include <stdio.h>
 
+int head (int N) {
+    int n = N;
+    printf("Funciona head");
+    return 0;
 }
 
 int tail (int N) {
-
+    int n = N;
+    printf("Funciona tail");
+    return 0;
 }
 
 int longlines (int N) {
-
+    int n = N;
+    printf("Funciona longlines");
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include "libreria.h"
 
 int main (int argc, char *argv[]) {
-
+    head(1);
+    tail(1);
+    longlines(1);
 }
